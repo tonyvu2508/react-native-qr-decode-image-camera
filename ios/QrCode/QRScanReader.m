a5eb720dd038351e244f0d9444896070bc5be047
tonyvu2508 <58989306+tonyvu2508@users.noreply.github.com>
2022-06-30 15:28:44 +0700

    Update QRScanReader.m

--- ios/QrCode/QRScanReader.m
+++ ios/QrCode/QRScanReader.m
@@ -2,12 +2,18 @@
 #import <AVFoundation/AVFoundation.h>
 #import <CoreImage/CoreImage.h>
 
+@import MLKitVision;
+@import MLImage;
+@interface QRScanReader()
+
+@end
 @implementation QRScanReader
+
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(readerQR:(NSString *)fileUrl success:(RCTPromiseResolveBlock)success failure:(RCTResponseErrorBlock)failure){
   dispatch_sync(dispatch_get_main_queue(), ^{
-    NSString *result = [self readerQR:fileUrl];
+    NSArray *result = [self readerQR:fileUrl];
     if(result){
       success(result);
     }else{
@@ -25,7 +31,7 @@ RCT_EXPORT_METHOD(readerQR:(NSString *)fileUrl success:(RCTPromiseResolveBlock)s
   
 }
 
--(NSString*)readerQR:(NSString*)fileUrl{
+-(NSArray*)readerQR:(NSString*)fileUrl{
   fileUrl = [fileUrl stringByReplacingOccurrencesOfString:@"file://" withString:@""];
   
   CIContext *context = [CIContext contextWithOptions:nil];
@@ -39,9 +45,62 @@ RCT_EXPORT_METHOD(readerQR:(NSString *)fileUrl success:(RCTPromiseResolveBlock)s
     return nil;
   }
   //3. Get scan results
-  CIQRCodeFeature *feature = [features objectAtIndex:0];
-  NSString *scannedResult = feature.messageString;
-  return scannedResult;
+//  CIQRCodeFeature *feature = [features objectAtIndex:0];
+//  NSString *scannedResult = feature.messageString;
+//  return scannedResult;
+    NSMutableArray *result = [[NSMutableArray alloc]init];
+    for (int i = 0; i<features.count; i++) {
+        CIQRCodeFeature *feature = [features objectAtIndex:i];
+        NSString *scannedResult = feature.messageString;
+        if(scannedResult.length>0){
+            [result addObject:scannedResult];
+        }
+        
+    }
+    return result;
+}
+
+- (void)onBarcodesDetected:(NSDictionary *)event
+{
+    
+}
+
+RCT_EXPORT_METHOD(readQRVision:(NSString *)fileUrl success:(RCTPromiseResolveBlock)success failure:(RCTResponseErrorBlock)failure){
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    NSArray *result = [self readQRVision:fileUrl];
+    if(result){
+      success(result);
+    }else{
+      NSString *domain = @"";
+      NSString *desc = NSLocalizedString(@"No related QR code", @"");
+      NSDictionary *userInfo = @{ NSLocalizedDescriptionKey : desc };
+      NSError *error = [NSError errorWithDomain:domain
+                                           code:404
+                                       userInfo:userInfo];
+      failure(error);
+    }
+  });
+  
+  
+  
+}
+
+-(NSArray*)readQRVision:(NSString*)fileUrl{
+    fileUrl = [fileUrl stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+    NSURL *url = [NSURL URLWithString:fileUrl];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    UIImage *img = [UIImage imageWithData:data];
+
+    BarcodeDetector * barcode = [[BarcodeDetector alloc] init];
+    NSArray<MLKBarcode *> *barcodes;
+    [barcode findBarcodesInImage:img completed:^(NSArray *barcodes) {
+        NSDictionary *eventBarcode = @{@"type" : @"barcode", @"barcodes" : barcodes};
+    }];
+    NSArray *arr1 = @[fileUrl];
+    return arr1;
+//    NSString *scannedResult = @"";
+//    scannedResult = [test objectAtIndex:0];
+//    return test;
 }
 
 @end
