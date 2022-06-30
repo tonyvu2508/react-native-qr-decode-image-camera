d6c978ddc6bc3fc3ffe528e22a0170dd3e17f4df
tonyvu2508 <58989306+tonyvu2508@users.noreply.github.com>
2022-06-30 15:29:10 +0700

    Update QRScanReader.h

--- ios/QrCode/QRScanReader.h
+++ ios/QrCode/QRScanReader.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
+#import "BarcodeDetector.h"
 
 @interface QRScanReader : NSObject<RCTBridgeModule>
-
+- (void)onBarcodesDetected:(NSDictionary *)event;
 @end
